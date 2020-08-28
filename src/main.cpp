1df3959a1f5daab505d6b703da143a2ef4229b8b
Jose <jzgonzales@gmail.com>
2020-08-27 19:43:47 -0500

    Se ajustan e integran las funciones de lectura y escritura, se prueba con hardware resultado OK

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,3 @@
-
-/* version 4.x del tablero de LEDS 8x8 que incorpora:
-* 8 circuitos de desplazamiento 74HC595 para escritura
-* 8 registros de desplazamiento 74LS165 para lectura
-* Utilizacion del protocolo SPI 
-* @autor: Jose Antonio Gonzales jzgonzales@gmail.com
-* @date: julio 2020
-*/
-
 /*
 ************************************************** Librerias ************************************************
 */
@@ -25,19 +16,35 @@ const byte _74HC165_01 = 7;  // LATCH-- Entrada carga en paralelo. Conecta el pi
 * La conexion en serie de multiples 74LS165 se hace del pin 10 del primer Chip al pin 9 del siguiente Chip sucesivamente.
 */
 
+//Asignacion de pines del 74HC595 que controla la funcion general de escritura
+int ss0 = 2; //Slave select fila 0
+int ss1 = 3; //Slave select fila 1
+int ss2 = 4; //Slave select fila 2
+int ss3 = 5; //Slave select fila 3
 
 /*
 ******************************************** Variables globales ********************************************
 */
 
 const int pixels = 10; // Cantidad de pixels por fila
-const int size = 16;
+const int bits = 16;
 
 #define t 1000 
 
 // Almacenamiento de los estados de los chips SPI de sensores-entradas
 
-int inputState [pixels] = { 0b0000000000,
+int dataMock [pixels] = {   0b1100000111,
+                            0b0010111000,
+                            0b0111000010,
+                            0b0111100110,
+                            0b0000000000,
+                            0b0000000000,
+                            0b0000000000,
+                            0b0000000000,
+                            0b0000000000,
+                            0b0000000000 };
+
+int dataInput [pixels] = {  0b0000000000,
                             0b0000000000,
                             0b0000000000,
                             0b0000000000,
@@ -48,168 +55,113 @@ int inputState [pixels] = { 0b0000000000,
                             0b0000000000,
                             0b0000000000 };
 
-int output [pixels] =    {  0b0000000000000000,
-                            0b0000000000000000,
-                            0b0000000000000000,
-                            0b0000000000000000,
-                            0b0000000000000000,
-                            0b0000000000000000,
-                            0b0000000000000000,
-                            0b0000000000000000,
-                            0b0000000000000000,
-                            0b0000000000000000 };
+int dataOutput [pixels] = { 0b0000000000,
+                            0b0000000000,
+                            0b0000000000,
+                            0b0000000000,
+                            0b0000000000,
+                            0b0000000000,
+                            0b0000000000,
+                            0b0000000000,
+                            0b0000000000,
+                            0b0000000000 };
 
-
-int dataArr[size] = { 0b0000001111111111,
-                      0b0000001111111110, 
-                      0b0000001111111100,
-                      0b0000001111111000,
-                      0b0000001111110000,
-                      0b0000001111100000,
-                      0b0000001111000000,
-                      0b0000001110000000,
-                      0b0000001100000000,
-                      0b0000001000000000,
-                      0b0000000000000000 };
-
-const int sizeA = 11;
 
 byte input_A;
+int delayTime = 500;
 String bitValue;
 
-int slaveSelectA = 7;
-int slaveSelectB = 6;
-int slaveSelectC = 5;
-int slaveSelectD = 4;
-
-int delayTime = 50;
-
 /*
-******************************************** Funciones Escritura ********************************************
+******************************************** Funciones de escritura ********************************************
 */
 
-void dataA()
+// Se escribe el valor a la n-fila de LEDs, recibe como parametro el valor del slave select -SPI de la fila y la posicion en el arreglo dataMock a imprimir
+void writeMock(int ss, int val)
 {
     SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
-    digitalWrite(slaveSelectA, LOW);            //Write our Slave select low to enable the SHift register to begin listening for data
-    SPI.transfer16(dataArr[0]);
-    digitalWrite(slaveSelectA, HIGH);          //Once the transfer is complete, set the latch back to high to stop the shift register listening for data
-    delay(delayTime);
+    digitalWrite(ss, LOW);            //Write our Slave select low to enable the SHift register to begin listening for data
+    SPI.transfer16(dataMock[val]);
+    digitalWrite(ss, HIGH);          //Once the transfer is complete, set the latch back to high to stop the shift register listening for data
     SPI.endTransaction();
 }
 
-void dataB()
+// Se escribe el valor a la n-fila de LEDs, recibe como parametro el valor del slave select -SPI de la fila y la posicion en el arreglo dataInput a imprimir
+void writeData(int ss, int val)
 {
     SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
-    digitalWrite(slaveSelectB, LOW);            //Write our Slave select low to enable the SHift register to begin listening for data
-    SPI.transfer16(dataArr[1]);
-    digitalWrite(slaveSelectB, HIGH);          //Once the transfer is complete, set the latch back to high to stop the shift register listening for data
-    delay(delayTime);
+    digitalWrite(ss, LOW);            //Write our Slave select low to enable the SHift register to begin listening for data
+    SPI.transfer16(dataInput[val]);
+    digitalWrite(ss, HIGH);          //Once the transfer is complete, set the latch back to high to stop the shift register listening for data
     SPI.endTransaction();
 }
 
-void dataC()
+// Se escribe el valor Negado a la n-fila de LEDs, recibe como parametro el valor del slave select -SPI de la fila y la posicion en el arreglo dataInput a imprimir
+void writeData_Comp(int ss, int val)
 {
     SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
-    digitalWrite(slaveSelectC, LOW);            //Write our Slave select low to enable the SHift register to begin listening for data
-    SPI.transfer16(dataArr[1]);
-    digitalWrite(slaveSelectC, HIGH);          //Once the transfer is complete, set the latch back to high to stop the shift register listening for data
-    delay(delayTime);
+    digitalWrite(ss, LOW);            //Write our Slave select low to enable the SHift register to begin listening for data
+    SPI.transfer16(~dataInput[val]);
+    digitalWrite(ss, HIGH);          //Once the transfer is complete, set the latch back to high to stop the shift register listening for data
     SPI.endTransaction();
 }
-
-void dataD()
-{
-    SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
-    digitalWrite(slaveSelectD, LOW);            //Write our Slave select low to enable the SHift register to begin listening for data
-    SPI.transfer16(dataArr[4]);
-    digitalWrite(slaveSelectD, HIGH);          //Once the transfer is complete, set the latch back to high to stop the shift register listening for data
-    delay(delayTime);
-    SPI.endTransaction();
-}
-
-
-
-void dataScan(int slave)
-{
-  for(int i = 0; i<11; i++)
-  {
-    SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
-    digitalWrite(slave, LOW);            //Write our Slave select low to enable the SHift register to begin listening for data
-    SPI.transfer16(dataArr[i]);
-    digitalWrite(slave, HIGH);          //Once the transfer is complete, set the latch back to high to stop the shift register listening for data
-    delay(delayTime);
-    SPI.endTransaction();
-  }
-
-}
-
-void dataScanInv(int slave)
-{
-  for(int i = 0; i<11; i++)
-  {
-    SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
-    digitalWrite(slave, LOW);            //Write our Slave select low to enable the SHift register to begin listening for data
-    SPI.transfer16(~dataArr[i]);
-    digitalWrite(slave, HIGH);          //Once the transfer is complete, set the latch back to high to stop the shift register listening for data
-    delay(delayTime);
-    SPI.endTransaction();
-  }
-
-}
-
-
 /*
-******************************************** Funciones Lectura ********************************************
+******************************************** Funciones de lectura ********************************************
 */
 
 // Lee y almacena el estado de cada valor entregado por los chips SPI a cada una las filas en el arreglo inputState
-void readStateSPI(){
+void readStateSPI()
+{
 
   SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
+  SPI.setBitOrder(MSBFIRST);
   // Inicializacion del estado de acuerdo con los pulsos requeridos por el chip
   digitalWrite (_74HC165_01, LOW); 
   digitalWrite (_74HC165_01, HIGH);
-
-  SPI.setBitOrder(MSBFIRST);
-  for(int i = 0; i < pixels; i++){
-    
+  
+  
+  for(int i = 0; i < pixels; i++)
+  {  
     // Se lee el estado del bus SPI y se almacena en cada posicion del arreglo
-    inputState[i] = SPI.transfer16(16);
+    dataInput[i] = SPI.transfer16(16);
 
-  }   
+  }
+
   Serial.println(" ");
   SPI.endTransaction();
+
 }
 
 void printState()
 {
-      // Imprime el valor decimal de la fila - Se puede usar para la etapa analitica de patrones
-    Serial.println((String)"SPI valor :" + inputState[0]);  // Se monitorea el estado de asignacion de cada fila
-    //Serial.println((String)"Bus SPI buffer " + i + " valor hexadecimal :" + (inputState[i], HEX) );  // Se monitorea el estado de asignacion de cada fila
+  // Imprime el valor decimal de la fila - Se puede usar para la etapa analitica de patrones
+  Serial.println((String)"SPI valor :" + dataInput[0]);  // Se monitorea el estado de asignacion de cada fila
+  //Serial.println((String)"Bus SPI buffer " + i + " valor hexadecimal :" + (inputState[i], HEX) );  // Se monitorea el estado de asignacion de cada fila
 }
 
 // Lee bit por bit el estado de cada pixel por filas y lo almacena en el arreglo state
-void readBitState(){
+void readBitState()
+{
 
-bool state [16] = { 0,0,0,0,0,0,0,0,0,0 };
+  bool state [pixels] = { 0,0,0,0,0,0,0,0,0,0 };
 
-    for(int i = 0; i < pixels; i++){
+    for(int i = 0; i < pixels; i++)
+    {
 
       Serial.print((String)"Estado bit x bit fila " + i + " : ");
       
-        for(int j = 0; j < pixels; j++){
-
-            state[i] = bitRead(inputState[i],j);
+        for(int j = 0; j < pixels; j++)
+        {
+            state[i] = bitRead(dataInput[i],j);
             Serial.print((String)state[i]);
         }
       Serial.print(" ");
-      Serial.print(inputState[i]);
+      Serial.print(dataInput[i]);
       Serial.println(" ");
 
     }
     
 }
+
 
 
 /*
@@ -219,49 +171,36 @@ bool state [16] = { 0,0,0,0,0,0,0,0,0,0 };
 //funcion de inicializacion general Arduino - Hardware y software
 
 void setup ()
-{
-
-  //inicializacion Variables lectura
-  SPI.begin ();
-
-  Serial.begin (115200);
+{ 
+  Serial.begin (115200);  // Inicializacion del monitor serial
   Serial.println ("Begin switch test.");
+  
+  //Inicializacion parametros modo lectura
+  SPI.begin (); // Inicializacion Protocolo SPI
+  SPI.setBitOrder(MSBFIRST); 
 
   //inicializacion pines chip lectura 
   pinMode (_74HC165_01, OUTPUT);
   digitalWrite (_74HC165_01, HIGH);
+  
+  //Inicializacion parametros hardware escritura
+  pinMode(ss0, OUTPUT);
+  pinMode(ss1, OUTPUT);
+  pinMode(ss2, OUTPUT);    
+  pinMode(ss3, OUTPUT);
 
-  //Inicializacion Variables escritura
-  pinMode(slaveSelectA, OUTPUT);
-  pinMode(slaveSelectB, OUTPUT);
-  pinMode(slaveSelectC, OUTPUT);    
-  pinMode(slaveSelectD, OUTPUT);
-
-  SPI.setBitOrder(MSBFIRST); 
 }
  
 //funcion ciclica Arduino 
 void loop ()
 {
-    /*
-  * Lectura LEDS
-  */
-  // Llamado a la funcion de lecto-escritura
-  //readAndWrite();
+
   readStateSPI();
   readBitState();
-  //printState();
 
-  /*
-  * Escritura LEDS
-  */
-  dataScanInv(slaveSelectA);
-  dataScan(slaveSelectB);
-  dataScanInv(slaveSelectB);  
-  dataScan(slaveSelectC);
-  dataScanInv(slaveSelectC);
-  dataScan(slaveSelectD);
-  dataScanInv(slaveSelectD); 
-
+  writeData_Comp(ss0, 0);
+  writeMock(ss1, 1);
+  writeMock(ss2, 2);
+  writeMock(ss3, 3);
 
 } 
